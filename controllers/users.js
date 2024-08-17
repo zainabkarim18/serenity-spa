f90505b86088577969cb65c289ec282b44f44bda
Maryam <maryammohsen202002861@gmail.com>
2024-08-17 12:33:47 +0300

    editing users controller

--- controllers/users.js
+++ controllers/users.js
@@ -5,31 +5,36 @@ const jwt = require('jsonwebtoken');
 
 // Models
 const User = require('../models/user');
+const { model } = require('mongoose')
 
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { username, email, password } = req.body;
 
-    const existingUser = await User.findOne({ username });
 
-    if (existingUser) {
-      return res.status(400).json({ error: 'Something went wrong, try again.' });
+    const existingUserByUsername = await User.findOne({ username });
+    const existingUserByEmail = await User.findOne({ email });
+
+    if (existingUserByUsername || existingUserByEmail) {
+      return res.status(400).json({ error: 'Username or Email already in use' });
     }
 
     const hashedPassword = bcrypt.hashSync(password, parseInt(process.env.SALT_ROUNDS));
+    const user = await User.create({ username, email, hashedPassword });
 
-    const user = await User.create({ username, hashedPassword });
 
     const token = jwt.sign(
       {
         id: user._id,
         username: user.username,
+        email: user.email,
       },
       process.env.JWT_SECRET
     );
 
+    
     return res.status(201).json({ user, token });
   } catch (error) {
     res.status(400).json({ error: 'Something went wrong, try again.' });
@@ -40,26 +45,31 @@ router.post('/signin', async (req, res) => {
   try {
     const { username, password } = req.body;
 
+    
     const existingUser = await User.findOne({ username });
 
     if (!existingUser) {
       return res.status(400).json({ error: 'Invalid Credentials' });
     }
 
+    
     const isValidPassword = bcrypt.compareSync(password, existingUser.hashedPassword);
 
     if (!isValidPassword) {
-      throw Error('Invalid Credentials');
+      return res.status(400).json({ error: 'Invalid Credentials' });
     }
 
+    
     const token = jwt.sign(
       {
         id: existingUser._id,
         username: existingUser.username,
+        email: existingUser.email,
       },
       process.env.JWT_SECRET
     );
 
+    
     return res.status(200).json({ user: existingUser, token });
   } catch (error) {
     res.status(400).json({ error: 'Something went wrong, try again.' });
--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-  password: {
+  hashedPassword: {
     type: String,
     required: true,
   },
