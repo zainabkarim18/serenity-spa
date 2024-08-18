c65dd51a8a3b26ddd08359a951bf137aa8b23af8
danahrb <danahbahar@gmail.com>
2024-08-18 10:05:47 +0300

    changed the review model

--- models/review.js
+++ models/review.js
@@ -15,10 +15,16 @@ const reviewSchema = new mongoose.Schema({
   rating: {
     type: Number,
     required: true,
+    min:1,
+    max:5,
   },
 
   comment: {
     type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
 });
 module.exports = mongoose.model("Review", reviewSchema);
